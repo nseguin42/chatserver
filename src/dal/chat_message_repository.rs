4407538ec7cbf08191a207d344cd68d60e844d99
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-02-12 17:59:23 -0600

    replace cargo-checkmate with custom CI pipeline so I can skip repo tests

--- src/dal/chat_message_repository.rs
+++ src/dal/chat_message_repository.rs
@@ -201,3 +201,61 @@ fn from_rows(rows: Vec<Row>) -> Vec<ChatMessage> {
 
     messages
 }
+
+#[cfg(test)]
+mod test {
+    use fake::{Fake, Faker};
+    use tokio::test;
+
+    use crate::models::chat_message::ChatMessage;
+
+    use super::*;
+
+    async fn setup() -> Result<ChatMessageRepository, Error> {
+        let config = Config::load("config.json").await?;
+        let mut repo = ChatMessageRepository::new(&config)?;
+        repo.connect().await?;
+
+        Ok(repo)
+    }
+
+    #[test]
+    async fn repo_add_message() -> Result<(), Error> {
+        let repo = setup().await?;
+        let message = Faker.fake::<ChatMessage>();
+        repo.add_message(&message).await?;
+
+        Ok(())
+    }
+
+    #[test]
+    async fn repo_get_messages_from_channel() -> Result<(), Error> {
+        let repo = setup().await?;
+
+        for _ in 0..10 {
+            let message = Faker.fake::<ChatMessage>();
+            repo.add_message(&message).await?;
+        }
+
+        let messages = repo
+            .get_messages_from_channel("test_channel", 10)
+            .await
+            .unwrap();
+
+        assert_eq!(messages.len(), 10);
+
+        Ok(())
+    }
+
+    #[test]
+    async fn repo_get_messages_from_user() -> Result<(), Error> {
+        let repo = setup().await?;
+        let message = Faker.fake::<ChatMessage>();
+        repo.add_message(&message).await?;
+        let messages = repo.get_messages_by_user(&message.username).await?;
+
+        assert_ne!(messages.len(), 0);
+
+        Ok(())
+    }
+}
